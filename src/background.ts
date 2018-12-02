16bd28d8aada190ea32cba58343bcbe277814b4a
cryptoquick <cryptoquick@gmail.com>
2018-12-02 13:16:50 -0700

    Add text upload, hashing, and local storage.

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 const { webRequest } = chrome
+import { sha3_512 } from 'js-sha3'
 
 const webRequestHandler = (details: any) => {
   const { url } = details
@@ -23,4 +24,20 @@ chrome.browserAction.onClicked.addListener(tab => {
   chrome.tabs.update(tab.id, {
     url: 'chrome-extension://gicmmlbnpjlcenhoncblehbkojdcnmon/index.html',
   })
+})
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(
+    sender.tab
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension',
+  )
+  if (request.type == 'addValue') {
+    const hash = sha3_512(request.payload)
+    chrome.storage.local.set({ [hash]: request.payload }, () => {
+      console.log(hash)
+      sendResponse({ response: 200, responseText: 'value added' })
+    })
+  }
+  return true
 })
--- src/components/UploadPanel.tsx
+++ src/components/UploadPanel.tsx
@@ -1,7 +1,60 @@
 import * as React from 'react'
 
-export const UploadPanel: React.SFC<{}> = ({}) => (
-  <div>
-    <p>upload</p>
-  </div>
-)
+export const UploadPanel: React.SFC<{}> = ({}) => {
+  const [formState, setFormState] = React.useState('default')
+  const [textState, setTextState] = React.useState('')
+
+  return (
+    <div>
+      <p>upload</p>
+      <button onClick={() => setFormState('text')}>upload text</button>
+      <button onClick={() => setFormState('file')}>upload file</button>
+      <div>
+        {formState === 'text' && (
+          <div>
+            <textarea
+              onChange={evt => {
+                evt.preventDefault()
+                const text = evt.currentTarget.value
+                setTextState(text)
+              }}
+            />
+            <button
+              onClick={evt => {
+                evt.preventDefault()
+                console.log(textState)
+                chrome.runtime.sendMessage(
+                  { type: 'addValue', payload: textState },
+                  response => {
+                    console.log(response)
+                  },
+                )
+              }}
+            >
+              submit
+            </button>
+          </div>
+        )}
+        {formState === 'file' && (
+          <div>
+            <input
+              type="file"
+              onChange={evt => {
+                evt.preventDefault()
+                // setTextState(evt.currentTarget.value)
+              }}
+            />
+            <button
+              onClick={evt => {
+                evt.preventDefault()
+                // console.log(textState)
+              }}
+            >
+              submit
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
